61e6439ce4aafb6cae9bd96392af50947f5df2fc
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-03-19 23:19:43 +0000

    setting the folder strattuer

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
 import express, { Request, Response, Application } from 'express';
 import bodyParser from 'body-parser';
 
-
-
-
 const app: Application = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// your routes and middleware go here
 
+// your routes and middleware go here
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  const name  =  " we are doing out best to make  things work"
+  res.send(name);
+
 });
 
 app.listen(process.env.PORT || 3000, () => {
