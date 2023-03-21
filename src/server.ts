067466d007c70257e9e239a62ce967ca021a79fb
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-03-21 12:10:00 +0000

    Adding of component

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,31 @@
-import express, { Request, Response, Application } from 'express';
-import bodyParser from 'body-parser';
+import express, { Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import imagesRoutes from './routes/ImageRoutes';
 
-const app: Application = express();
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-// your routes and middleware go here
-app.get('/', (req: Request, res: Response) => {
-  const name  =  " we are doing out best to make  things work"
-  res.send(name);
-
+// Connect to the MongoDB database
+mongoose.connect('mongodb://localhost/imageUploader', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server listening on port ${process.env.PORT || 3000}`);
+// Set up middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set up routes
+app.use('/api/images', imagesRoutes);
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Error handling middleware
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Internal server error');
 });
+
+// Start the server
+app.listen(port, () =>
