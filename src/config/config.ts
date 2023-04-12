853c94eb465761ceb437112accd112c9bb02da7d
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-04-12 20:48:15 +0000

    file updated

--- src/config/config.ts
+++
@@ -1,9 +0,0 @@
-// config.ts
-
-const config = {
-    port: process.env.PORT || 3000,
-    mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/image-uploader"
-  };
-  
-  export default config;
-  
