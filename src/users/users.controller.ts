4acc16f4dead3b93edfa6e9d80335356a2564f27
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-05-26 18:17:34 +0000

    adding of user deleting accounf and logout

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,7 @@ import {
   Post,
   UseGuards,
   Request,
+  Delete,
 } from '@nestjs/common';
 
 import * as bcrypt from 'bcrypt';
@@ -12,7 +13,7 @@ import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
 import { LocalAuthGuard } from 'src/auth/local.auth.guard';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
@@ -21,20 +22,16 @@ export class UsersController {
   async addUser(
     @Body('password') userPassword: string,
     @Body('username') userName: string,
-  ) 
-  {
+  ) {
     //hash password
     const saltOrRounds = 10;
     const hashedPassword = await bcrypt.hash(userPassword, saltOrRounds);
 
-    const result = await this.usersService.insertUser(
-      userName,
-      hashedPassword,
-    );
+    const result = await this.usersService.insertUser(userName, hashedPassword);
     return {
       msg: 'User successfully registered',
       userId: result.id,
-      userName: result.username
+      userName: result.username,
     };
   }
 
@@ -42,21 +39,37 @@ export class UsersController {
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   login(@Request() req): any {
-    return {User: req.user,
-            msg: 'User logged in'};
-  };
+    // return the user id and username
+    return {
+      msg: 'User successfully logged in',
+      userId: req.user.userId,
+      userName: req.user.userName,
+    };
+  }
 
   //Get / protected
   @UseGuards(AuthenticatedGuard)
   @Get('/protected')
   getHello(@Request() req): string {
     return req.user;
-  };
+  }
 
   //Get / logout
   @Get('/logout')
-    logout(@Request() req): any {
-      req.session.destroy()
-      return { msg: 'The user session has ended' }
-    }
+  logout(@Request() req): any {
+    req.session.destroy();
+
+    return { msg: 'The user session has ended' };
+  }
+
+  //Deleting  users  account
+  @UseGuards(AuthenticatedGuard)
+  @Delete('/delete')
+  async deleteUser(@Request() req): Promise<any> {
+    const userId = req.user.userId; // Extract the userId from req.user
+    const result = await this.usersService.deleteUser(userId);
+    return {
+      msg: 'User successfully deleted',
+    };
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './users.model';
@@ -6,7 +6,7 @@ import { User } from './users.model';
 @Injectable()
 export class UsersService {
   constructor(@InjectModel('user') private readonly userModel: Model<User>) {}
- 
+
   //Signup user method with username and password
   async insertUser(userName: string, password: string) {
     const username = userName.toLowerCase();
@@ -22,5 +22,19 @@ export class UsersService {
     const username = userName.toLowerCase();
     const user = await this.userModel.findOne({ username });
     return user;
+  }
+
+  // Delete user
+  async deleteUser(userId: string) {
+    const result = await this.userModel.deleteOne({ _id: userId }).exec();
+    if (result.deletedCount === 0) {
+      throw new NotFoundException('Could not find user.');
+    }
+    // return result;
+
+    return {
+      msg: 'User successfully deleted',
+      userId: userId,
+    };
   }
 }
